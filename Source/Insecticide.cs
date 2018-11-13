19e6f618f44a581de688053a2516c65f89f863f6
EPSIL0N <diablo_man0@hotmail.com>
2018-11-13 17:48:43 -0500

    Add files via upload

---
+++ Source/Insecticide.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Insecticide
+{
+    public class DamageWorker_AddBugInjury : DamageWorker_AddGlobal
+    {
+        
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+            Pawn pawn = thing as Pawn;
+            if (pawn != null)
+            {
+                Hediff hediff = HediffMaker.MakeHediff(dinfo.Def.hediff, pawn, (BodyPartRecord) null);
+                hediff.Severity = Rand.Range(.01f, pawn.RaceProps.FleshType == FleshTypeDefOf.Insectoid ? 1f : 0.05f);
+                pawn.health.AddHediff(hediff, (BodyPartRecord) null, new DamageInfo?(dinfo), (DamageWorker.DamageResult) null);
+            }
+            return new DamageWorker.DamageResult();
+        }
+    }
+}
